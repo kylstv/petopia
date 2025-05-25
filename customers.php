aade8d327a8f6f30f50916d2e89fdad2b317a058
kylstv <kyleoriana@gmail.com>
2025-05-25 23:15:52 +0800

    1

--- customers.php
+++ customers.php
@@ -1,16 +1,5 @@
 <?php
-// Database connection
-$host = 'localhost';
-$dbname = 'kitter';
-$username = 'root';
-$password = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+include "connect.php";
 
 // Verify admin password function
 function verifyAdminPassword($pdo, $password) {
