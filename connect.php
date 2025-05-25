9e18af9027c0eb23a8d23398eeaf4b42ec875722
kylstv <kyleoriana@gmail.com>
2025-05-25 22:14:29 +0800

    q

--- connect.php
+++ connect.php
@@ -1,13 +1,17 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "kitter";  // Assigning the database name to the variable
-$conn = new mysqli($host, $user, $pass, $db);  // Use $db to specify the database
-
-if ($conn->connect_error) {
-    echo "Failed to connect DB: " . $conn->connect_error;
+$serverName = $_SERVER['SERVER_NAME'] ?? 'localhost';
+if ($serverName === 'localhost' || $serverName === '127.0.0.1') {
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'kitter';
+} else {
+    $db_host = 'localhost';
+    $db_user = 'u801377270_petopiaph_2025';
+    $db_pass = 'Petopiaph_2025';
+    $db_name = 'u801377270_petopiaph_2025';
 }
-
-?>
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
