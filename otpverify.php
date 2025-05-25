caf4c112d29b769fbfca4724a94bbbbb6deed5a7
kylstv <kyleoriana@gmail.com>
2025-05-25 22:40:17 +0800

    1

--- otpverify.php
+++ otpverify.php
@@ -1,13 +1,11 @@
 <?php
+
 $email = $_GET['email'] ?? '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $otp = $_POST['otp'];
 
-    $conn = new mysqli("localhost", "root", "", "kitter");
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    include "connect.php";
 
     $sql = "SELECT * FROM signup WHERE email = ? AND otp = ?";
     $stmt = $conn->prepare($sql);
